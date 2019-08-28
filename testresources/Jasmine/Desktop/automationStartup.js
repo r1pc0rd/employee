41d241a6c6df394c4746cdb318d0840665b92929
Dinesh Reddy <dinesh.gioe@gmail.com>
2019-08-28 18:33:22 +0530

    Update automationStartup.js

--- testresources/Jasmine/Desktop/automationStartup.js
+++ testresources/Jasmine/Desktop/automationStartup.js
@@ -1,27 +1,3 @@
-describe("Smoke Test", function() {
-	beforeEach(function() {
-		//gets executed once before each spec in the describe 
-      // Could run Login Here
-     //  require(['testScripts/Accounts_Login'], function(){
-  	//ß	jasmine.getEnv().execute();
-	//});
-	});
-	
-	afterEach(function() {
-		//gets executed once after each spec.
-      //Could Run Lopgout Here
-       //require(['testScripts/Accounts_Logout'], function(){
-  		//jasmine.getEnv().execute();
-	//});
-      
-	});
-	
-	it("Checks Account", function() {
-      require(['testScripts/Accounts_Login','testScripts/Account_LIstDetail','testScripts/Accounts_Logout','testScripts/Account_CheckAlex'], function(){
-  		jasmine.getEnv().execute();
-	});
-		
-	}); 
-				  
-});
-
+require(['testScripts/Accounts_Login','testScripts/Accounts_LIstDetail','testScripts/Accounts_Logout'], function(){
+	jasmine.getEnv().execute();		
+}); 
